07b714b5eebaa996f04e78e92c44172531d9f6c8
Nick Sherman <ogleffeckt2@yahoo.com>
2019-08-26 16:54:09 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -1,32 +1,41 @@
-jQuery(document).ready(function() {
-  
-  var btn = $('#button');
+// jQuery(document).ready(function() {
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
-    } else {
-      btn.removeClass('show');
-    }
-  });
+//   var btn = $('#button');
 
-  btn.on('click', function(e) {
+//   $(window).scroll(function() {
+//     if ($(window).scrollTop() > 300) {
+//       btn.addClass('show');
+//     } else {
+//       btn.removeClass('show');
+//     }
+//   });
+
+//   btn.on('click', function(e) {
+//     e.preventDefault();
+//     $('html, body').animate({scrollTop:0}, '300');
+//   });
+
+// });
+
+
+
+
+document.querySelector("form").onsubmit = function (e) {
     e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
-  });
 
-});
+    let movieName = document.querySelector("#navbarSupportedContent > form > input").value;
 
-let movieName = 'gangs of new york'
+    axios.get(`http://www.omdbapi.com/?apikey=cb06939&s=${movieName}`).then((result) => {
+        console.log(result)
 
-axios.get(`http://www.omdbapi.com/?apikey=cb06939&s=${movieName}`).then((result) => {
-    console.log(result)
+        // axios.get(`http://www.omdbapi.com/?apikey=cb06939&i=${result.data.Search[0].imdbID}`).then(result2=>{
+        //     console.log(result2)
+        // }).catch((err) => {console.log(err)});
 
-    axios.get(`http://www.omdbapi.com/?apikey=cb06939&i=${result.data.Search[0].imdbID}`).then(result2=>{
-        console.log(result2)
-    }).catch((err) => {console.log(err)});
-}).catch((err) => {console.log(err)});
-
+    }).catch((err) => {
+        console.log(err)
+    });
+}
 
 
 document.querySelector("#navbarSupportedContent > form > input").unclick
