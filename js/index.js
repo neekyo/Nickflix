9f35e0a7ac7e8169e8b8e07333dc154205532e9a
Nick Sherman <ogleffeckt2@yahoo.com>
2019-08-27 13:57:11 -0400

    deploy

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-// jQuery(document).ready(function() {
-
-//   var btn = $('#button');
-
-//   $(window).scroll(function() {
-//     if ($(window).scrollTop() > 300) {
-//       btn.addClass('show');
-//     } else {
-//       btn.removeClass('show');
-//     }
-//   });
-
-//   btn.on('click', function(e) {
-//     e.preventDefault();
-//     $('html, body').animate({scrollTop:0}, '300');
-//   });
-
-// });
-
 document.querySelector("form").onsubmit = function (e) {
     e.preventDefault();
 
@@ -35,3 +16,55 @@ document.querySelector("form").onsubmit = function (e) {
 }
 
 document.querySelector("#navbarSupportedContent > form > input").unclick
+
+const myList = document.querySelectorAll("#s1 img");
+const action = document.querySelectorAll("#s2 img");
+const thrillers = document.querySelectorAll("#s3 img");
+const horror = document.querySelectorAll("#s4 img");
+const animated = document.querySelectorAll("#s5 img");
+const comedies = document.querySelectorAll("#s6 img");
+const tvShows = document.querySelectorAll("#s7 img");
+const recentlyAdded = document.querySelectorAll("#s8 img");
+
+
+for(let i = 1; i< myList.length; i++){
+    myList[i].onclick = getOneRegister;
+}
+for(let i = 1; i< action.length; i++){
+    action[i].onclick = getOneRegister;
+}
+for(let i = 1; i< thrillers.length; i++){
+    thrillers[i].onclick = getOneRegister;
+}
+for(let i = 1; i< horror.length; i++){
+    horror[i].onclick = getOneRegister;
+}
+for(let i = 1; i< animated.length; i++){
+    animated[i].onclick = getOneRegister;
+}
+for(let i = 1; i< comedies.length; i++){
+    comedies[i].onclick = getOneRegister;
+}
+for(let i = 1; i< tvShows.length; i++){
+    tvShows[i].onclick = getOneRegister;
+}
+for(let i = 1; i< recentlyAdded.length; i++){
+    recentlyAdded[i].onclick = getOneRegister;
+}
+
+// function getOneRegister(e) {
+//     let id = Number(e.target.alt);
+//     console.log(id)
+//     axios.get("http://localhost:8000/movies/" +id)
+//       .then((response) => {
+//         console.log(response.data)
+//       }).catch(err => console.error(err))
+//   }
+function getOneRegister(e) {
+    let id = Number(e.target.alt);
+    console.log(id)
+    axios.get("../db.json")
+      .then((response) => {
+        console.log(response.data)
+      }).catch(err => console.error(err))
+  }
